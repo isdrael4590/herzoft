419402c3a33bc8b4cd62cc45480c099e109b6cfd
José Jácome <josejacomeb@gmail.com>
2024-06-15 22:14:30 -0500

    WIP: Corregido captura, tratando de enviar

--- Modules/Labelqr/Http/Controllers/PrinterLabelQrController.php
+++ Modules/Labelqr/Http/Controllers/PrinterLabelQrController.php
@@ -18,6 +18,8 @@ use Modules\Labelqr\Entities\LabelqrDetails;
 use Modules\Labelqr\Http\Requests\StoreReceptionRequest;
 use SnappyImage;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Http;
+
 class PrinterLabelQrController extends Controller
 {
     public function printerLabelqr(Int $id)
@@ -53,7 +55,37 @@ class PrinterLabelQrController extends Controller
     {   
         $urlEtiqueta = route("labelqrs_label.html", $id);
         # Permite guardar la imagen
-        $image = SnappyImage::generate($urlEtiqueta, storage_path('image.png'));
-        return redirect()->route('labelqrs_label.pdf', $id);
+        $img_filename = 'etiqueta_'. $id .'.png';
+        $labelqr = Labelqr::where('id', $id)->first();
+        $labelqrDetails = LabelqrDetails::with('product')
+            ->where('id', $id)
+            ->orderBy('id', 'DESC')
+            ->get();
+        $institute = Institute::all()->first();
+        $label_qrs_print = view('labelqr::labelqrs.print-only', [
+            'labelqr' => $labelqr,
+            'labelqrDetails' => $labelqrDetails,
+            'institute' => $institute,
+        ]);
+        $image_view = SnappyImage::loadView('labelqr::labelqrs.print-only', [
+            'labelqr' => $labelqr,
+            'labelqrDetails' => $labelqrDetails,
+            'institute' => $institute,
+        ]);
+        $image = $image_view->stream($img_filename);
+        // Get the client's IP address
+        $clientIp = request()->ip();
+        // Define the local server URL to send the image to
+        $clientServerUrl = "http://$clientIp:3000/";
+
+        // Send the image to the local server
+        $response = Http::withHeaders([
+            'Content-Type' => 'image/png',
+        ])->post($clientServerUrl, [
+            'image' => base64_encode($image),
+            'data' => "hi",
+        ]);
+        dump($response);
+        #return redirect()->route('labelqrs_label.pdf', $id);
     }
 }
