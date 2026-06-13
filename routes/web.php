690f00bca22426e992e20a7e4752e7af58f451f1
José Jácome <josejacomeb@gmail.com>
2026-06-12 22:28:15 -0500

    tarea: actualizar a Laravel 12.x y PHP 8.4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController; // Added modern controller import
 
 /*
 |--------------------------------------------------------------------------
@@ -14,17 +16,31 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/home', 'HomeController@index')
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])
         ->name('home');
 
-    Route::get('/testbowies/chart-data', 'HomeController@testBowiesChart')
+    Route::get('/testbowies/chart-data', [HomeController::class, 'testBowiesChart'])
         ->name('testbowies.chart');
-    Route::get('/current-month/chart-data', 'HomeController@currentMonthChart')->name('current-month.chart');
-    Route::get('/currentProducMonth/chart-data', 'HomeController@currentMonthProductionChart')->name('currentProduc-month.chart');
-    Route::get('/productions/chart-data', 'HomeController@ProductionsChart')->name('productions.chart');
-    Route::get('/productionlabels/chart-data', 'HomeController@ProductionlabelsChart')->name('productionlabels.chart');
-    Route::get('/resultproductions/chart-data', 'HomeController@ResultProductionChart')->name('resultproductions.chart');
-    Route::get('/biologics/chart-data', 'HomeController@BiologicChart')->name('biologics.chart');
-    Route::get('/central/chart-data', 'HomeController@CentralChart')->name('central.chart');
+
+    Route::get('/current-month/chart-data', [HomeController::class, 'currentMonthChart'])
+        ->name('current-month.chart');
+
+    Route::get('/currentProducMonth/chart-data', [HomeController::class, 'currentMonthProductionChart'])
+        ->name('currentProduc-month.chart');
+
+    Route::get('/productions/chart-data', [HomeController::class, 'ProductionsChart'])
+        ->name('productions.chart');
+
+    Route::get('/productionlabels/chart-data', [HomeController::class, 'ProductionlabelsChart'])
+        ->name('productionlabels.chart');
+
+    Route::get('/resultproductions/chart-data', [HomeController::class, 'ResultProductionChart'])
+        ->name('resultproductions.chart');
+
+    Route::get('/biologics/chart-data', [HomeController::class, 'BiologicChart'])
+        ->name('biologics.chart');
+
+    Route::get('/central/chart-data', [HomeController::class, 'CentralChart'])
+        ->name('central.chart');
 });
