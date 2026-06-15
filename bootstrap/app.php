c6f6b2fca8d650359068a6003b34f5e143b2d6ac
isdrael4590 <isdrael4590@gmail.com>
2026-06-14 21:24:41 -0500

    mejorar middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'force.password.change' => \App\Http\Middleware\ForcePasswordChange::class,
+        ]);
+        $middleware->appendToGroup('web', [
+            \App\Http\Middleware\CheckLicenceExpiration::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
