f483a2d4d9539b9b99372944d44d978f26bcdb82
José Jácome <josejacomeb@gmail.com>
2026-06-26 21:57:08 -0500

    arreglo: lints en los .sh

--- scripts/diario.sh
+++ scripts/diario.sh
@@ -15,7 +15,8 @@ DIAS_GUARDAR_ARCHIVOS=45
 # Previene que se enmascare errores
 set -euo pipefail
 # Carga las variables del archivo .env
-source $RUTA_PROYECTO/.env
+# shellcheck source=/dev/null
+source "$RUTA_PROYECTO/.env"
 echo "Tratando de guardar $FECHA"
 # Lee la contraseña del archivo secreto
 MYSQL_PASSWORD=$(<"$ARCHIVO_CONTRASENA")
@@ -23,7 +24,7 @@ MYSQL_PASSWORD=$(<"$ARCHIVO_CONTRASENA")
 mkdir -p "$RUTA_RESPALDO"
 echo "Obtener respaldo del usuario $DB_USERNAME y base de datos $DB_DATABASE"
 # Guardar base de datos
-docker exec "$NOMBRE_CONTENEDOR" /usr/bin/mysqldump -u"$DB_USERNAME" -p"$MYSQL_PASSWORD" "$DB_DATABASE" > "$NOMBRE_ARCHIVO"
+docker exec "$NOMBRE_CONTENEDOR" /usr/bin/mysqldump -u"$DB_USERNAME" -p"$MYSQL_PASSWORD" "$DB_DATABASE" >"$NOMBRE_ARCHIVO"
 
 echo "Comprimir el respaldo con el password..."
 zip -P "$CONTRASENA_RESPALDO" "$RUTA_RESPALDO/$NOMBRE_ZIP" "$NOMBRE_ARCHIVO" >/dev/null
--- scripts/quincenal.sh
+++ scripts/quincenal.sh
@@ -11,18 +11,25 @@ RUTA_REMOTA="${CARPETA_REMOTA}/${ANHO}/${MES}"
 # Previene que se enmascare errores
 set -euo pipefail
 # Carga las variables del archivo .env
-source $RUTA_PROYECTO/.env
+# shellcheck source=/dev/null
+source "$RUTA_PROYECTO/.env"
 
 echo "Subiendo archivos de respaldo a GDrive en la ruta $RUTA_REMOTA ..."
 rclone mkdir "$RUTA_REMOTA" || true
 
 echo "Selecionando el archivo más reciente..."
-ARCHIVO_RECIENTE=$(ls -t "$RUTA_RESPALDO"/*.zip | head -n 1)
+ARCHIVO_RECIENTE=""
+for archivo in "$RUTA_RESPALDO"/*.zip; do
+    # Si no existen archivos .zip, el bucle podría tomar el string literal '*.zip', lo controlamos aquí:
+    [[ -e "$archivo" ]] || break
+    # Compara si este archivo es más reciente (-nt = newer than) que el guardado previamente
+    [[ -z "$ARCHIVO_RECIENTE" || "$archivo" -nt "$ARCHIVO_RECIENTE" ]] && ARCHIVO_RECIENTE="$archivo"
+done
 echo "Archivo seleccionado: $ARCHIVO_RECIENTE"
 
 if [ -z "$ARCHIVO_RECIENTE" ]; then
-    echo "[ERROR] No hay archivos de respaldo para subir."
-    exit 1
+	echo "[ERROR] No hay archivos de respaldo para subir."
+	exit 1
 fi
 
 echo "Subiendo $ARCHIVO_RECIENTE a GDrive..."
